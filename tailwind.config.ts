5d79740c4880875974ba2a6a6ab95315abca44ba
Mateusz Lewandowski <mlewandowski@creatuity.com>
2023-09-07 23:11:02 +0200

    fix tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from 'tailwindcss';
 
-const tailwindConfig: Config = {
+const config: Config = {
 	content: [
 		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
 		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -16,5 +16,4 @@ const tailwindConfig: Config = {
 	},
 	plugins: [],
 };
-
-export { tailwindConfig };
+export default config;
