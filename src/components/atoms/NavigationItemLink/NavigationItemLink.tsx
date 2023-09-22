5b02d9da027736db12b074ef6dc4b5b77a447d4c
Mateusz Lewandowski <mlewandowski@creatuity.com>
2023-09-23 01:09:33 +0200

    [2.10] fix navigation item

--- src/components/atoms/NavigationItemLink/NavigationItemLink.tsx
+++ src/components/atoms/NavigationItemLink/NavigationItemLink.tsx
@@ -6,16 +6,22 @@ import { usePathname } from 'next/navigation';
 import { type NavigationItemLinkProps } from '@components/atoms';
 
 export const NavigationItemLink = (props: NavigationItemLinkProps) => {
-	const { name, href, partialMatching = false } = props;
+	const {
+		name,
+		href,
+		className = 'font-medium text-gray-950 hover:text-gray-800',
+		activeClassName = 'font-semibold underline',
+		exact = false,
+	} = props;
 	const pathname = usePathname();
-	const path = partialMatching ? `/${pathname.split('/')[1]}` : pathname;
+	const path = exact ? `/${pathname.split('/')[1]}` : pathname;
 	const isActive = path === href;
 
 	return (
 		<Link
 			href={{ pathname: href }}
-			className={clsx(`font-medium text-gray-950 hover:text-gray-800`, {
-				'font-semibold underline': isActive,
+			className={clsx(className, {
+				[activeClassName]: isActive,
 			})}
 			aria-current={isActive ? 'page' : undefined}
 		>
